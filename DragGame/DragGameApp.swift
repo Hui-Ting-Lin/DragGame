45ba93853cad9551e6c9dadf0933bfa646b718ef
Chase <kristen880319@gmail.com>
2021-03-29 21:04:20 +0800

    Initial Commit

---
+++ DragGame/DragGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGameApp.swift
+//  DragGame
+//
+//  Created by Chase on 2021/3/29.
+//
+
+import SwiftUI
+
+@main
+struct DragGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
